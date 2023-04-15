6162bf7d8e40686bfc43ec55b67d6306a1e3b154
AbrarQ <104406765+AbrarQ@users.noreply.github.com>
2023-04-15 13:12:46 +0530

    Lets start coding

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import ExpenseItem from './components/ExpenseItem'
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>This is also visible</p>
+      <p>This is so fast</p>
+
+   <ExpenseItem>
+   </ExpenseItem>
+
+    </div>
+
+
+
+  )
+}
+
+export default App;
